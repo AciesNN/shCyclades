c2ea834a3c156321792f6a5760586855d5eddcc6
Acies <acies4spam@mail.ru>
2014-08-15 17:29:20 +0400

    Код сетки киклад

--- Assets/Game/Scripts/UI/Map/UIMapCyclades.cs
+++ Assets/Game/Scripts/UI/Map/UIMapCyclades.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class UIMapCyclades : UIMapHexController {
 
-	public int CycladGridY {
-		get { return (YSize + 1) / 2; }
-	}
+	public int CycladGridY;
+	public int CycladGridX;
 
 	void Awake() {
+		YSize = CycladGridY * 2 - 1;
+		XSize = CycladGridX + CycladGridY - 1;
 		HexGridType = HexGridOrientation.HORIZONTAL;
 	}
 
@@ -21,7 +22,7 @@ public class UIMapCyclades : UIMapHexController {
 	
 	public override Vector3 CellToWorldPosition(GridPosition pos) {
 
-		GridPosition cell = pos; //CycladesCoordToCell(pos);
+		GridPosition cell = CycladesCoordToCell(pos);
 		Vector3 res = base.CellToWorldPosition(cell);
 
 		return res;
@@ -30,9 +31,8 @@ public class UIMapCyclades : UIMapHexController {
 
 	public override bool IsCellPossible(GridPosition cell) {
 
-		GridPosition pos = CellToCycladesCoord(cell);
-
-		return true;
+		return cell.x >= 0 && cell.y >= 0 && cell.y < YSize
+			&& cell.x < CycladGridX + CycladGridY - 1 - System.Math.Abs(cell.y + 1 - CycladGridY); //	10 - (6 - 6)
 
 	}
 
@@ -40,13 +40,10 @@ public class UIMapCyclades : UIMapHexController {
 	GridPosition CycladesCoordToCell(GridPosition cell) {
 		GridPosition res;
 		
-		//1. учтем перевернутый ыгрик 
+		// учтем перевернутый ыгрик 
 		res = new GridPosition(cell.x, (CycladGridY - 1) - cell.y); //-1 потому что: например, высота 13, координата 0, надо получить 12, т.к. индексация с нуля
-		
-		//2. учтем бордюры
-		res = new GridPosition(res.x + 1, res.y - 1); //+ и - потому, что в одном случае мы отступаем от начала координат, а потом - от конца (координаты Киклад идут от верхнего угла, а сетки - от нижнего)
-		
-		//3. учтем разную длину разных линий
+
+		// учтем разную длину разных линий
 		res = new GridPosition(res.x + System.Math.Abs(cell.y - 5)/2, res.y);
 		
 		return res;
@@ -55,13 +52,10 @@ public class UIMapCyclades : UIMapHexController {
 	GridPosition CellToCycladesCoord(GridPosition cell) {
 		GridPosition res = cell;
 		
-		//2. учтем перевернутый ыгрик 
+		// учтем перевернутый ыгрик 
 		res = new GridPosition(res.x, (CycladGridY - 1) - res.y);
 		
-		//2. учтем бордюры
-		res = new GridPosition(res.x - 1, res.y - 1); 
-		
-		//3. учтем разную длину разных линий
+		// учтем разную длину разных линий
 		res = new GridPosition(res.x - System.Math.Abs(res.y - 5)/2, res.y);
 		
 		return res;
--- Assets/Game/Scripts/UI/Map/Util/Drive/UIMapDraggerAndPresser.cs
+++ Assets/Game/Scripts/UI/Map/Util/Drive/UIMapDraggerAndPresser.cs
@@ -3,13 +3,14 @@
 public class UIMapDraggerAndPresser : MonoBehaviour
 {
 	public UIDraggableCamera draggableCamera;
-	public UIMapHexController MapController;
+	public UIMapController MapController;
 
 	void OnClick ()	{
 
 		Vector3 pos = draggableCamera.camera.ScreenToWorldPoint(Input.mousePosition) / 0.003125f; //волшебное число - scale UIRoot NGUI по-умолчанию
 		GridPosition cell = MapController.WorldPositionToCell(pos);
-		NGUIDebug.Log("press cell: " + cell); 
+		if (MapController.IsCellPossible(cell))
+			NGUIDebug.Log("press cell: " + cell); 
 
 	}
 
