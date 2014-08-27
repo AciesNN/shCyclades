fc451462ff3d61e4d76fffc2a60f97d2bf633018
AciesNN <acies4spam@mail.ru>
2014-08-27 23:39:41 +0400

    -

--- Assets/Game/Scripts/UI/Panels/Gods/UIGodPoseidonPanel.cs
+++ Assets/Game/Scripts/UI/Panels/Gods/UIGodPoseidonPanel.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class UIGodPoseidonPanel : UIGamePanel {
 
+	#region ViewWidgets
+	public GameObject BuyButtonsStrip;
+	#endregion
+	
+	#region Events
 	public void OnBuyUnitClick() {
 		Sh.GameState.mapStates.SetType(MapEventerType.PLACESHIP);
 	}
@@ -18,4 +23,13 @@ public class UIGodPoseidonPanel : UIGamePanel {
 	public void OnEndTurn() {
 		Sh.Out.Send("end turn");
 	}
+
+	public void OnBuyUnitsClick() {
+		OpenCloseBuyButton(!BuyButtonsStrip.activeSelf);
+	}
+
+	void OpenCloseBuyButton(bool open) {
+		BuyButtonsStrip.SetActive(open);
+	}
+	#endregion
 }
