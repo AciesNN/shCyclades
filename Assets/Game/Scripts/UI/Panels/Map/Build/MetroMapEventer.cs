4818bca781606c0911de3ac66ab11058caa3e433
Acies <acies4spam@mail.ru>
2014-09-16 17:16:52 +0400

    - поправлено строительство метрополии по зданиям (без выбора какие сносить) - добавлены сейвы для теста этого

--- Assets/Game/Scripts/UI/Panels/Map/Build/MetroMapEventer.cs
+++ Assets/Game/Scripts/UI/Panels/Map/Build/MetroMapEventer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 using Cyclades.Game;
 using Cyclades.Game.Client;
@@ -17,7 +18,7 @@ class MetroMapEventer: IslandClickMapEventer {
 		if (Sh.GameState.currentUser != -1) { //todo совершенно лишнее в реальной игре условие
 			List<long> islands = Library.Map_GetIslandsByOwner(Sh.In.GameContext, Sh.GameState.currentUser);
 			foreach (long island in islands) {
-				//if (Sh.In.GameContext.GetLong("/map/islands/owners/[{0}]", island) == Sh.GameState.currentUser)
+				if (!Sh.In.GameContext.GetBool("/map/islands/is_metro/[{0}]", island))
 					allowedIslands.Add(island);
 			}
 		}
@@ -30,12 +31,36 @@ class MetroMapEventer: IslandClickMapEventer {
 
 	#region Abstract
 	override protected void OnClickIsland(int island) {
-		if (Metro4Buildings)
-			Sh.Out.Send( Messanges.PlaceMetro4Buildings(island, new List<object>())); //TODO слоты уничтожаемых зданий 
+		if (Metro4Buildings) {
+			List<object> slots_and_islands = GetSlotsForMetro();
+			Sh.Out.Send( Messanges.PlaceMetro4Buildings(island, slots_and_islands)); 
+		}
 		else 
 			Sh.Out.Send( Messanges.PlaceMetro4Philosopher(island) );
 		mapStates.SetEventorType(MapEventerType.DEFAULT);
 	}
 	#endregion
 
+	List<object> GetSlotsForMetro() {
+		//TODO тут конечно нужно как-то дать выбор
+
+		List<object> slots_and_islands = new List<object>();
+		List<string> buildings = new List<string>();
+
+		List<object> owners = Sh.In.GameContext.GetList ("/map/islands/owners");
+		for(int island = 0; island < buildings.Count; ++island) {
+			if ((int)(long)owners[island] == Sh.GameState.currentUser) {
+				List<object> slots = Sh.In.GameContext.GetList ("/map/islands/buildings/[{0}]", island);
+				for(int s = 0; s < slots.Count; ++s) {
+					string slot = slots[s] as string;
+					if (slot != Cyclades.Game.Constants.buildNone && buildings.IndexOf(slot) >= 0) {
+						buildings.Add (slot);
+						slots_and_islands.Add ( new List<object> {island, s} );
+					}
+				}
+			}
+		}		
+
+		return slots_and_islands;
+	}
 }
